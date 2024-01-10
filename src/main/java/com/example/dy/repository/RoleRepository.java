9479fd35043b99e22fbb6d82077a41cafe693be9
김정호 <jhjhjh1919@daum.net>
2024-01-10 18:49:13 +0900

    게시판 만들기

--- src/main/java/com/example/dy/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.dy.repository;
-
-import com.example.dy.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-
-    Role findByName(String name);
-}
