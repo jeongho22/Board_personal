736b16a066895750f7763d5c3fc14563599e0248
김정호 <jhjhjh1919@daum.net>
2024-03-22 00:41:07 +0900

    TimeZone

--- src/main/java/com/example/dy/DyApplication.java
+++ src/main/java/com/example/dy/DyApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class DyApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul")); // 시간대 설정
 		SpringApplication.run(DyApplication.class, args);
 	}
 
